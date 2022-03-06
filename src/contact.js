07dc18f52f4ee8a4266f90212c8bdf772dfc626f
h-nriquevieira <h.nriquevieira@gmail.com>
2022-03-06 11:43:05 -0300

    Structure Contact page

--- src/contact.js
+++ src/contact.js
@@ -1,13 +1,24 @@
 function createMenuPage() {
   const header = document.createElement('h1');
   const copy = document.createElement('p');
+  const contactForm = document.createElement('form');
+  const nameLabel = document.createElement('label');
+  const nameInput = document.createElement('input');
+  const messageLabel = document.createElement('label');
+  const messageInput = document.createElement('textarea');
 
-  header.textContent = 'Bread Company';
-  copy.textContent = 'Bread Company was created in 2022 to provide fresh products to you every day.';
+  header.textContent = 'Contact Us';
+  copy.textContent = 'We love hearing from our clients!';
+  nameLabel.textContent = 'Name:';
+  nameLabel.htmlFor = 'name';
+  nameInput.id = 'name';
+  messageLabel.textContent = 'Message:';
+  messageLabel.htmlFor = 'message';
+  messageInput.id = 'message';
 
+  contactForm.append(nameLabel, nameInput, messageLabel, messageInput);
 
-
-  return [header, copy];
+  return [header, copy, contactForm];
 }
 
 export {createMenuPage as default};
